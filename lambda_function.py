2ef43be752d42a6a5f1d88d53fb534d89c3871f5
Shiv Rathod <shivr12400@gmail.com>
2024-09-18 19:49:58 -0400

    All endpoints working

--- lambda_function.py
+++ lambda_function.py
@@ -8,11 +8,13 @@ from boto3.dynamodb.conditions import Key
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 leader_info_table = dynamodb.Table('leader-info')
 satsang_count_table = dynamodb.Table('satsang-count')
-upcoming_events_table = dynamodb.Table('upcoming_events')
+upcoming_events_table = dynamodb.Table('upcoming-events')
+kids_table = dynamodb.Table('kids-list')
 
 leader_info = '/leaderInfo'
 satsang_count = '/satsangCount'
 upcoming_events = '/upcomingEvents'
+kids = '/kids'
 
 def lambda_handler(event, context):
     print('Request event: ', event)
@@ -27,16 +29,17 @@ def lambda_handler(event, context):
         elif http_method == 'POST' and path == leader_info:
             return post_leader_info(json.loads(event['body'])) 
         elif http_method == 'GET' and path == satsang_count:
-            mandir_name = event['queryStringParameters']['mandirName']
-            date = event['queryStringParameters']['date']
-            return get_satsang_count(mandir_name, date)
+            return get_satsang_count()
         elif http_method == 'POST' and path == satsang_count:
             return post_satsang_count(json.loads(event['body']))
         elif http_method == 'GET' and path == upcoming_events:
-            mandir_name = event['queryStringParameters']['mandirName']
-            return get_upcoming_events(mandir_name)
+            return get_upcoming_events()
         elif http_method == 'POST' and path == upcoming_events:
             return post_upcoming_events(json.loads(event['body']))
+        elif http_method == 'GET' and path == kids:
+            return get_kids()
+        elif http_method == 'POST' and path == kids:
+            return post_kid(json.loads(event['body']))
         else:
             return build_response(404, '404 Not Found')
 
@@ -65,13 +68,25 @@ def post_leader_info(request_body):
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
 
-def get_satsang_count(mandir_name, date):
+def get_satsang_count():
     try:
-        response = satsang_count_table.get_item(Key={'mandirName': mandir_name, "date": date})
-        return build_response(200, response.get('Item'))
+        scan_params = {
+            'TableName': satsang_count_table.name
+        }
+        return build_response(200, scan_dynamo_records_satsang_counts(scan_params, []))
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
+
+def scan_dynamo_records_satsang_counts(scan_params, item_array):
+    response = satsang_count_table.scan(**scan_params)
+    item_array.extend(response.get('Items', []))
+   
+    if 'LastEvaluatedKey' in response:
+        scan_params['ExclusiveStartKey'] = response['LastEvaluatedKey']
+        return scan_dynamo_records(scan_params, item_array)
+    else:
+        return {'satsang_count': item_array}
 
 def post_satsang_count(request_body):
     try:
@@ -86,17 +101,63 @@ def post_satsang_count(request_body):
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
     
-def get_upcoming_events(mandir_name):
+def get_upcoming_events():
     try:
-        response = upcoming_events_table.get_item(Key={'mandirName': mandir_name})
-        return build_response(200, response.get('Item'))
+        scan_params = {
+            'TableName': upcoming_events_table.name
+        }
+        return build_response(200, scan_dynamo_records_events(scan_params, []))
     except ClientError as e:
         print('Error:', e)
         return build_response(400, e.response['Error']['Message'])
 
+
 def post_upcoming_events(request_body):
     try:
         upcoming_events_table.put_item(Item=request_body)
+        body = {
+            'Operation': 'SAVE',
+            'Message': 'SUCCESS',
+            'Item': request_body
+        }
+        return build_response(200, body)
+    except ClientError as e:
+        print('Error:', e)
+        return build_response(400, e.response['Error']['Message'])
+        
+def get_kids():
+    try:
+        scan_params = {
+            'TableName': kids_table.name
+        }
+        return build_response(200, scan_dynamo_records(scan_params, []))
+    except ClientError as e:
+        print('Error:', e)
+        return build_response(400, e.response['Error']['Message'])
+
+def scan_dynamo_records(scan_params, item_array):
+    response = kids_table.scan(**scan_params)
+    item_array.extend(response.get('Items', []))
+   
+    if 'LastEvaluatedKey' in response:
+        scan_params['ExclusiveStartKey'] = response['LastEvaluatedKey']
+        return scan_dynamo_records(scan_params, item_array)
+    else:
+        return {'kids': item_array}
+        
+def scan_dynamo_records_events(scan_params, item_array):
+    response = upcoming_events_table.scan(**scan_params)
+    item_array.extend(response.get('Items', []))
+   
+    if 'LastEvaluatedKey' in response:
+        scan_params['ExclusiveStartKey'] = response['LastEvaluatedKey']
+        return scan_dynamo_records(scan_params, item_array)
+    else:
+        return {'upcomingEvents': item_array}
+
+def post_kid(request_body):
+    try:
+        kids_table.put_item(Item=request_body)
         body = {
             'Operation': 'SAVE',
             'Message': 'SUCCESS',
@@ -111,7 +172,9 @@ def build_response(status_code, body):
     return {
         'statusCode': status_code,
         'headers': {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Credentials" : 'true',
         },
         'body': json.dumps(body, cls=DecimalEncoder)
     }
